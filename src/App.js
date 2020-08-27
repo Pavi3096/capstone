b0ae7adcefac506268fb7fc0d99b931df8921202
Pavithra <rpavithra3096@gmail.com>
2020-08-27 19:19:31 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import Weather from "./Weather";
+import News from "./News";
+import ToDo from "./ToDo";
+
+
+
+function App() {
+  return (
+  <main>
+    <h1> Hey Pavi! </h1>
+      <Weather/>
+    <section>
+      <div>
+        <h3>Latest headlines</h3>
+        <News/>
+      </div>
+      <div>
+        <h3>Today's Agenda</h3>
+      <ToDo/>
+      </div>
+    </section>
+  </main>
+  );
+}
+
+export default App;
